46498c8d0e96a501c14ab032abbd7b814abc925d
Venuspatel711 <160982867+Venuspatel711@users.noreply.github.com>
2024-03-19 23:56:34 +0000

    added something

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Program.classes; 
+public class ProgramGeneric<T>
+{
+        public T X;
+        
+        public ProgramGeneric(T param)
+        {
+        X = param;
+        }
+ public string? GetString()
+ {
+        return X?.ToString();
+ }
+
+
+
+}
